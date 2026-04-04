a0e655e372aaaa0e9bdf666045cc558194823c82
obozdag <obozdag@gmail.com>
2026-04-04 17:07:07 +0200

    docs(release): publish v2.3.9 asset references
    - retarget docs and playground asset loaders to the new v2.3.9 CDN tag instead of master
    - update visible docs and playground version strings plus package integration snippets for the new release

--- playground/webblocks-assets.js
+++ playground/webblocks-assets.js
@@ -2,9 +2,9 @@
   'use strict';
 
   var assets = {
-    css: 'https://cdn.jsdelivr.net/gh/fklavyenet/webblocks-ui@master/packages/webblocks/dist/webblocks-ui.css',
-    icons: 'https://cdn.jsdelivr.net/gh/fklavyenet/webblocks-ui@master/packages/webblocks/dist/webblocks-icons.css',
-    js: 'https://cdn.jsdelivr.net/gh/fklavyenet/webblocks-ui@master/packages/webblocks/dist/webblocks-ui.js'
+    css: 'https://cdn.jsdelivr.net/gh/fklavyenet/webblocks-ui@v2.3.9/packages/webblocks/dist/webblocks-ui.css',
+    icons: 'https://cdn.jsdelivr.net/gh/fklavyenet/webblocks-ui@v2.3.9/packages/webblocks/dist/webblocks-icons.css',
+    js: 'https://cdn.jsdelivr.net/gh/fklavyenet/webblocks-ui@v2.3.9/packages/webblocks/dist/webblocks-ui.js'
   };
 
   var head = document.head;
